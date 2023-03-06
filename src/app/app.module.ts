e1758ba9fa2663cfeb8e1cf965b0d56ec7762958
eyaMattoussi <eya.mattoussii@gmail.com>
2023-03-06 18:10:23 +0100

    add ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
@@ -36,7 +36,8 @@ import { ShiftComponent } from './shift/shift.component';
     AppRoutingModule,
     NgbModule ,
     FormsModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
